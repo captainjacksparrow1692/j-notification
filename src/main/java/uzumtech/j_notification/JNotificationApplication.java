e1d2749fec507be154c2761f18e7c0cf1f3313d3
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-30 00:43:01 +0500

    first commit

---
+++ src/main/java/uzumtech/j_notification/JNotificationApplication.java
@@ -0,0 +1,13 @@
+package uzumtech.j_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uzumtech/j_notification/JNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package uzumtech.j_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
